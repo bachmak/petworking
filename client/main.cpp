0e76711597209738f30c05888dafaf950de441c2
bachmak <kochetov6969@outlook.com>
2022-05-06 16:04:23 +0000

    Изменил std::clog на std::cerr

--- client/main.cpp
+++ client/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const* argv[]) {
   auto context = ese::Context();
-  auto logger = ese::Logger(std::clog, std::cin);
+  auto logger = ese::Logger(std::cerr, std::cin);
 
   auto [host_name, host_port] = ese::ReadNameWithPort(argc, argv);
   auto client = ese::Client(context, host_name, host_port, logger);
