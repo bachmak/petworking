ce9e6e4a6768bffe15b25dfd987f4ae2385150b1
GavryaAK <alina.gavrya@mail.ru>
2022-05-08 11:54:34 +0000

    chore(ConnectionSettings): изменен регистр параметра protocol

--- common/connection_settings.h
+++ common/connection_settings.h
@@ -9,7 +9,7 @@ enum class Protocol { TCP, UDP };
 struct ConnectionSettings {
   ConnectionSettings(std::string_view protocol, std::string_view host,
                      std::string_view port) {
-    this->protocol = protocol == "UDP" ? Protocol::UDP : Protocol::TCP;
+    this->protocol = protocol == "udp" ? Protocol::UDP : Protocol::TCP;
     this->host = Ip::from_string(host.data());
     this->port = boost::lexical_cast<Port>(port);
   }
