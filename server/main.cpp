8eb79274f87385ffcad0ddf880d09edc77391e73
bachmak <kochetov6969@outlook.com>
2022-05-09 11:28:31 +0000

    refactor(main): переработы main.cpp клиента и сервера
    работа с аргументами командной строки в main через CommandLineOptions

--- server/main.cpp
+++ server/main.cpp
@@ -1,13 +1,20 @@
+#include "common/command_line_options.h"
+#include "common/connection_settings.h"
 #include "common/logger.h"
-#include "common/utils.h"
 #include "server/server_factory.h"
 
 int main(int argc, char const* argv[]) {
   try {
+    auto cmd_options = ese::CommandLineOptions(argc, argv);
+
+    if (cmd_options.Help()) {
+      std::cout << cmd_options << std::endl;
+      return 0;
+    }
+
+    auto settings = ese::ConnectionSettings(cmd_options);
     auto context = ese::Context();
     auto logger = ese::Logger(std::cerr, std::cin);
-
-    auto settings = ese::ReadConnectionSettings(argc, argv);
 
     auto server = ese::CreateServer(context, settings, logger);
 
